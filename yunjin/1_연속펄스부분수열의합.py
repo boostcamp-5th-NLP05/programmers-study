7514343df288ca25f8f63776a5245e81a3cc07fa
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-05-23 20:46:45 +0900

    [Fix] 통과 풀이 추가

--- yunjin/1_연속펄스부분수열의합.py
+++ yunjin/1_연속펄스부분수열의합.py
@@ -1,3 +1,46 @@
+# 통과 풀이
+# (출처 : https://lottegiantsv3.tistory.com/168)
+def solution(sequence):
+    answer = -1e9
+
+    dp = [[0, 0] for _ in range(len(sequence))]
+
+    dp[0][0], dp[0][1] = sequence[0] * -1, sequence[0]
+
+    answer = max(answer, max(dp[0]))
+
+    for idx, seq in enumerate(sequence):
+        if idx == 0:
+            continue
+
+        # 펄스 수열이므로 계속 바꿔줘야 한다.
+        if idx % 2 == 0:
+            dp[idx][0] = max(dp[idx - 1][0], 0) + sequence[idx] * -1
+            dp[idx][1] = max(dp[idx - 1][1], 0) + sequence[idx]
+
+        else:
+            dp[idx][0] = max(dp[idx - 1][0], 0) + sequence[idx]
+            dp[idx][1] = max(dp[idx - 1][1], 0) + sequence[idx] * -1
+
+        answer = max(answer, max(dp[idx]))
+
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 본인 시도 코드
 def first_check(i, sequence, dp, fulse):
     plus_start = sequence[i - 1] - sequence[i]
     minus_start = -sequence[i - 1] + sequence[i]
