4c8bd2b15e42d0a70d423fda925e41c6928f667d
darae <dahye.stat@gmail.com>
2023-05-31 16:29:40 +0900

    Update 0531_숫자게임.py

--- dahye/0531_숫자게임.py
+++ dahye/0531_숫자게임.py
@@ -3,9 +3,9 @@ def solution(A, B):
     A.sort(reverse = True)
     B.sort(reverse = True)
     for i in range(len(A)):
-        if max(B) > A[i]: # 현재 A의 가장 큰 값보다 B가 크면 B 1점 획득
+        if B[0] > A[i]: # 현재 A의 가장 큰 값보다 B가 크면 B 1점 획득
             answer += 1
-            B.remove(max(B))
+            B.pop(0)
         else:
             B.pop() #B의 가장 작은 값을 제외하여 최대한 많은 승점 획득
 
