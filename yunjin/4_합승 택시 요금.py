43280a25197249d526c0297737347ddeb0396d0f
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-06-02 12:48:38 +0900

    yunjin-0602

---
+++ yunjin/4_합승 택시 요금.py
@@ -0,0 +1,57 @@
+import heapq  # 우선순위 큐 구현을 위함
+import copy
+from collections import deque
+
+
+# def dijkstra(start):
+#     q=[]
+#     #시작 노드로 가기 위한 최단 경로는 0으로 설정하여 큐에 삽입
+#     heapq.heappush(q, (0, start))
+#     distance[start]=0
+
+#     #q가 비어있지 않다면
+#     while q:
+#         #가장 최단 거리인 노드에 대한 정보 꺼내기
+#         dist, now = heapq.heappop(q)
+#         #현재 노드가 이미 처리됐다면 skip
+#         if distance[now] < dist:
+#             continue
+#         #현재 노드와 연결된 다른 인접한 노드 확인
+#         for i in graph[now]:
+#             cost = dist + i[1]
+#             #현재 노드를 거치면 이동 거리가 더 짧은 경우
+#             if cost < distance[i[0]]:
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
+
+
+def solution(n, s, a, b, fares):
+    answer = 0
+
+    graph = [[] for i in range(n + 1)]
+
+    print(graph)
+    for start, end, dis in fares:
+        graph[start].append([end, dis])
+        graph[end].append([start, dis])
+
+    visited = [False for i in range(n)]
+
+    print(graph)
+
+    d = graph.deepcopy()
+
+    d = deque()
+    d.append([s, 0])
+
+    while d:
+
+        s1, d1 = d.popleft()
+
+        if not visited[s1]:
+            visited[s1] = True
+
+            for e2, d2 in graph[s1]:
+                d.append([e2, d1 + d2])
+
+    return answer
