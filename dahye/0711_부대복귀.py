723b3e6625e55d90dcbe3d80cbe343d2eaf7873c
darae <dahye.stat@gmail.com>
2023-07-13 04:51:26 +0900

    Update 0711_부대복귀.py
    다익스트라 알고리즘으로 다시 풀이

--- dahye/0711_부대복귀.py
+++ dahye/0711_부대복귀.py
@@ -1,37 +1,36 @@
+import heapq
+
 def solution(n, roads, sources, destination):
+    inf = 1e9
     answer = []
-    INF = int(1e9)
+    graph = [[] for _ in range(n+1)]
+    distance = [inf] * (n+1)
 
-    # 2차원 리스트 (그래프 표현) 만들고, 무한대로 초기화
-    graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
-
-    # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            if a == b:
-                graph[a][b] = 0
-
-    # 각 간선에 대한 정보를 입력받아, 그 값으로 초기화
     for road in roads:
-        # A -> B로 가는 비용을 C라고 설정
-        a, b = road
-        graph[a][b] = 1
-        graph[b][a] = 1 # 서로 이어졌으므로
+        a,b = road
+        graph[a].append(b)
+        graph[b].append(a)
 
-    # 점화식에 따라 플로이드 워셜 알고리즘을 수행
-    for k in range(1, n + 1):
-        for a in range(1, n + 1):
-            for b in range(1, n + 1):
-                graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    # 다익스트라 알고리즘 수행
+    q = []
+    heapq.heappush(q, (0,destination))
+    distance[destination] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + 1
+            if cost < distance[i]: # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+                distance[i] = cost
+                heapq.heappush(q, (cost, i))
 
     # destination에서 s까지 방문의 최소시간
     for s in sources:
-        ans = graph[destination][s]
-        if ans >= INF:
-            answer.append(-1)
+        if distance[s] != inf:
+            answer.append(distance[s])
         else:
-            answer.append(ans)
+            answer.append(-1)
     
     return answer
 
@@ -40,4 +39,3 @@ roads = [[1, 2], [1, 4], [2, 4], [2, 5], [4, 5]]
 sources = [1,3,5]
 destination = 5
 print(solution(n, roads, sources, destination))
-
