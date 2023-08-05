45d4d7dc2f716ea851921c3773981dd0f7a1ef0a
darae <dahye.stat@gmail.com>
2023-08-05 21:47:31 +0900

    Update 선입선출스케줄링.py

--- dahye/선입선출스케줄링.py
+++ dahye/선입선출스케줄링.py
@@ -6,23 +6,22 @@ def solution(n, cores):
         return n
 
     core_list = []
-    temp_core_list = []
-    for i in range(len(cores)): #코어 번호 반영 및 작은 코어부터 처리하기 위함
-        temp_core_list.append([cores[i], i+1]) # 코어 넣기
-    temp_core_list.sort()
+    for i in range(len(cores)):
+        heapq.heappush(core_list, (cores[i], cores[i], i+1)) # 코어 넣기
+    temp_n = 0
 
-    for t in range(len(temp_core_list)):
-        a, b = temp_core_list[t]
-        heapq.heappush(core_list, (0, a, b)) # 코어 넣기
-    temp_n = 1
+    n -= len(cores)
+
     while True:
         # temp_n 증가 n이 되면 멈춤
         temp_core, core, core_num = heapq.heappop(core_list)
-        if temp_n == n:
-            return core_num
+        
     
         temp_n += 1
         temp_core += core
+
+        if temp_n == n:
+            return core_num
         heapq.heappush(core_list, (temp_core, core, core_num))
         
         
